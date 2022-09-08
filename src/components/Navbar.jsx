92edeb3dd27bf4aa7c9126e883d87f244a29a1b3
Renier Arias Granda <renier.arias@gmail.com>
2022-09-08 00:46:08 -0400

    [wip] layout

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,103 @@
+import React, { useState } from 'react'
+import {
+  AppBar,
+  Avatar,
+  Badge,
+  Box,
+  InputBase,
+  Menu,
+  MenuItem,
+  styled,
+  Toolbar,
+  Typography,
+} from '@mui/material'
+import { LogoDev, Mail, Notifications } from '@mui/icons-material'
+
+const StyledToolbar = styled(Toolbar)({
+  display: 'flex',
+  justifyContent: 'space-between',
+})
+
+const Search = styled('div')(({ theme }) => ({
+  backgroundColor: 'white',
+  padding: '0 10px',
+  borderRadius: theme.shape.borderRadius,
+  width: '40%',
+}))
+
+const Icons = styled(Box)(({ theme }) => ({
+  display: 'none',
+  gap: '20px',
+  alignItems: 'center',
+  [theme.breakpoints.up('sm')]: {
+    display: 'flex',
+  },
+}))
+
+const UserBox = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  gap: '10px',
+  alignItems: 'center',
+  [theme.breakpoints.up('sm')]: {
+    display: 'none',
+  },
+}))
+
+const Navbar = () => {
+  const [open, setOpen] = useState(false)
+
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
+
+  return (
+    <AppBar position="sticky">
+      <StyledToolbar>
+        <Typography variant="h6" sx={{ display: { xs: 'none', sm: 'block' } }}>
+          LearnyLib
+        </Typography>
+        <LogoDev sx={{ display: { xs: 'block', sm: 'none' } }} />
+        <Search>
+          <InputBase placeholder="Search..." />
+        </Search>
+        <Icons>
+          <Badge badgeContent={2} color="error">
+            <Mail />
+          </Badge>
+          <Badge badgeContent={4} color="error">
+            <Notifications />
+          </Badge>
+          <Avatar
+            src="/assets/001-man.svg"
+            sx={{ height: 30, width: 30 }}
+            onClick={handleOpen}
+          />
+        </Icons>
+        <UserBox onClick={handleOpen}>
+          <Avatar src="/assets/001-man.svg" sx={{ height: 30, width: 30 }} />
+          <Typography variant="span">John Doe</Typography>
+        </UserBox>
+      </StyledToolbar>
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        open={open}
+        onClose={handleClose}
+        onClick={handleClose}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+      >
+        <MenuItem>Profile</MenuItem>
+        <MenuItem>My account</MenuItem>
+        <MenuItem>Logout</MenuItem>
+      </Menu>
+    </AppBar>
+  )
+}
+
+export default Navbar
---
+++ src/theme.js
@@ -0,0 +1,11 @@
+import { createTheme } from '@mui/material'
+
+export const theme = createTheme({
+  palette: {
+    secondary: {
+      light: '#d3d3d3',
+      main: '#a6a6a6',
+      dark: '#656565'
+    }
+  }
+})
